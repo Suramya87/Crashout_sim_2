20b64b88c57a877bcbde297a318f7c1a87640c58
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-06-04 23:07:39 -0700

    ZOOMMMMMM

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { popupTask } from './tasks/popupTask.js';
+import { zoomTask } from './tasks/zoomTask.js'; 
+
 
 const difficultyRatings = {
   A: 10,
@@ -10,7 +12,7 @@ let currentRating = 'A';
 let pointCap = difficultyRatings[currentRating];
 let activePoints = 0;
 
-const taskPool = [popupTask];
+const taskPool = [popupTask,zoomTask];
 
 function trySpawnTask() {
   const task = taskPool[Math.floor(Math.random() * taskPool.length)];
